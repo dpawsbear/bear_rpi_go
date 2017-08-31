cf416a9758f189cc8ae19976cf42110c13b2e0b9
dpawsbear <dpawsbear@gmail.com>
2017-08-31 20:27:03 +0800

    success change package

--- example/gpio.go
+++ example/gpio.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
+	rpi "github.com/dpawsbear/bear_rpi_go"
 )
 
+
 func main(){
-	fmt.Println("Hello world")
+
+	fmt.Println("test for gpio 12 ")
+
+	fmt.Println("starting...")
+	if 0 != rpi.Bcm2837_init() {
+		rpi.Bcm2837_close()
+		panic(55)
+		return
+	}
+
+	rpi.Bcm2837_gpio_fsel(rpi.RPI_3B_GPIO_J8_12,uint8(rpi.BCM2837_GPIO_FSEL_OUTP))
+	rpi.Bcm2837_gpio_set_pud(rpi.RPI_3B_GPIO_J8_12, rpi.BCM2837_GPIO_PUD_UP)
+	rpi.Bcm2837_gpio_set(rpi.RPI_3B_GPIO_J8_12)
+	time.Sleep(time.Second * 3)
+	rpi.Bcm2837_gpio_clr(rpi.RPI_3B_GPIO_J8_12)
+
+	fmt.Println("test finish")
+
 }
